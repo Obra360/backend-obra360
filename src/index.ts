6f1514063a1f7a21c5b317fa806f4b146b110d8e
DiegoSCC <94144545+DiegoSCC@users.noreply.github.com>
2025-07-22 07:22:59 +0000

    Created post and get methods for authenticatation

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,83 @@
 import express from "express";
-import obrasRouter from '/home/toby/Obra360/backend-obra360/src/routes/obras.js';
 import path from "path";
 import cors from "cors";
-import pkg from '@prisma/client';
+import pkg, { User } from '@prisma/client';
 import { fileURLToPath } from "url";
+import { hash } from "crypto";
+import { sign } from "jsonwebtoken";
+import { compare } from "bcryptjs";
+import { authenticate, ExpressRequest } from "./middlewares/auth.js";
+//  corregido referencia local de router en obras
+import router from "./routes/obras.js";
 
 const { PrismaClient } = pkg;
+const prisma = new PrismaClient();
 const app = express();
 const PORT = 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/obras', obrasRouter);
-const prisma = new PrismaClient();
-
+const generateJwt = (user: User): string  => {
+  return sign({email: user.email}, 'JWT_SECRET')
+}
 //  Necesario para que __dirname funcione con ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 //  Ruta al frontend
 const frontendPath = path.join(__dirname, "../../frontend-obra360");
 
-//  Servir archivos estáticos (CSS, JS, imágenes)
-app.use(express.static(frontendPath));
 
-// Ruta para "/"
-app.get("/", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
+app.use(cors());
+app.use(express.json());
+app.use('/api/obras', router);
+
+
+//  Metodo para crear usuarios con constraseña hasheada
+app.post("/users", async (req, res) =>{
+  try {
+    const hashedPassword = await hash(req.body.password, 10);
+    const user = await prisma.user.create({
+      data: {...req.body, password: hashedPassword}
+    });
+    const {password: _password, ...userWithoutPassword} = user
+    res.json({...userWithoutPassword, token: generateJwt(user)});
+  } catch (err) {
+    res.json({ error: "Email or username is not unique" });
+  }
 });
 
-//  Ruta comodín para servir cualquier .html directamente
-app.get("/*.html", (req, res) => {
-  const requestedFile = path.join(frontendPath, req.path);
-  res.sendFile(requestedFile, (err) => {
-    if (err) {
-      res.status(404).send("Archivo no encontrado");
+
+app.post('users/login', async (req, res)  =>{
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (!user) {
+      throw new Error('User not found')
     }
-  });
+
+    const isPasswordCorrect = await compare(req.body.password, user.password)
+
+    if (!isPasswordCorrect) {
+      throw new Error('Incorrect password')
+    }
+  } catch (error) {
+    res.json({error: 'Email or password are wrong'})
+  }
 });
+
+//  Metodo para autenticacion a traves de middleware authenticate
+app.get('/user', authenticate, async (req: ExpressRequest, res, next) =>{
+  try {
+    if(!req.user) {
+      return res.sendStatus(401)
+    }
+    const {password: _password, ...userWithoutPassword} = req.user;
+    res.json({...userWithoutPassword, token: generateJwt(req.user)});
+  } catch (err) {
+    next(err);
+  }
+})
 
 
 app.listen(PORT, () => {
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,41 @@
+import { User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/extension";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+
+const prisma = new PrismaClient();
+
+export interface ExpressRequest extends Request {
+  user?: User;
+}
+
+export const authenticate = async (
+  req: ExpressRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  if (!req.headers.authorization) {
+    throw new Error("Unauthorized");
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    throw new Error("Token not found");
+  }
+
+  try {
+    const decode = verify(token, 'JWT_SECRET') as { email: string };
+    const user = await prisma.user.findUnique({
+        where: {
+            email: decode.email,
+        }
+    });
+    req.user = user ?? undefined;
+    next();
+  } catch (err) {
+    req.user = undefined;
+    next();
+  }
+};
